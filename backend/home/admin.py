993f4ecd7f9b2d4cc91585b732ea99195d96ec9b
crowdbotics-dev <engineering@crowdbotics.com>
2023-11-07 08:12:15 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import FFFsefe
+
+admin.site.register(FFFsefe)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class FFFsefe(models.Model):
+    "Generated Model"
+    dfde = models.BigIntegerField()
